3f26fb9b72db088614e89fae75c2a613552e2d59
Paul Dix <paul@pauldix.net>
2009-12-18 15:23:53 -0500

    added require 'uri' to fix bug

--- lib/domainatrix.rb
+++ lib/domainatrix.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
 
+require 'uri'
 require 'domainatrix/domain_parser.rb'
 require 'domainatrix/url.rb'
 
