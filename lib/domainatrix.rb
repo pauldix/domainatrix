2b7991faf8011cc982262da013cb163e615dd155
Paul Dix <paul@pauldix.net>
2010-01-07 15:53:35 -0500

    bumped version for build

--- lib/domainatrix.rb
+++ lib/domainatrix.rb
@@ -5,7 +5,7 @@ require 'domainatrix/domain_parser.rb'
 require 'domainatrix/url.rb'
 
 module Domainatrix
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 
   def self.parse(url)
     @domain_parser ||= DomainParser.new("#{File.dirname(__FILE__)}/effective_tld_names.dat")
