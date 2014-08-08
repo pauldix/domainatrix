1fadf64ae73926eb863c1f7c88a743751a3e96d3
Sebastian Sogamoso <sebasoga@gmail.com>
2014-08-08 00:44:18 -0500

    Convert specs to RSpec 2.99.1 syntax with Transpec
    This conversion is done by Transpec 2.3.6 with the following command:
        transpec --keep its
    
    * 51 conversions
        from: obj.should
          to: expect(obj).to
    
    * 44 conversions
        from: == expected
          to: eq(expected)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/domainatrix_spec.rb
+++ spec/domainatrix_spec.rb
@@ -2,16 +2,16 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe "domainatrix" do
   it "should parse into a url object" do
-    Domainatrix.parse("http://pauldix.net").should be_a Domainatrix::Url
+    expect(Domainatrix.parse("http://pauldix.net")).to be_a Domainatrix::Url
   end
 
   it "should canonicalize" do
-    Domainatrix.parse("http://pauldix.net").canonical.should == "net.pauldix"
-    Domainatrix.parse("http://pauldix.net/foo.html").canonical.should == "net.pauldix/foo.html"
-    Domainatrix.parse("http://pauldix.net/foo.html?asdf=bar").canonical.should == "net.pauldix/foo.html?asdf=bar"
-    Domainatrix.parse("http://foo.pauldix.net").canonical.should == "net.pauldix.foo"
-    Domainatrix.parse("http://foo.bar.pauldix.net").canonical.should == "net.pauldix.bar.foo"
-    Domainatrix.parse("http://pauldix.co.uk").canonical.should == "uk.co.pauldix"
+    expect(Domainatrix.parse("http://pauldix.net").canonical).to eq("net.pauldix")
+    expect(Domainatrix.parse("http://pauldix.net/foo.html").canonical).to eq("net.pauldix/foo.html")
+    expect(Domainatrix.parse("http://pauldix.net/foo.html?asdf=bar").canonical).to eq("net.pauldix/foo.html?asdf=bar")
+    expect(Domainatrix.parse("http://foo.pauldix.net").canonical).to eq("net.pauldix.foo")
+    expect(Domainatrix.parse("http://foo.bar.pauldix.net").canonical).to eq("net.pauldix.bar.foo")
+    expect(Domainatrix.parse("http://pauldix.co.uk").canonical).to eq("uk.co.pauldix")
   end
 
   context 'localhost with a port' do
