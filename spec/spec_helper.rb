0374c4def8b75f7814ce3f4148619b4e79f4f2f9
Sebastian Sogamoso <sebasoga@gmail.com>
2014-08-08 00:42:07 -0500

    Bump RSpec version and add rspec-its RSpec needed to be upgraded to void getting this error when installing the dependencies: ``` An error occurred while installing rspec-expectations (2.7.0), and Bundler cannot continue. Make sure that `gem install rspec-expectations -v '2.7.0'` succeeds before bundling. ```
    Adding rspec-it was necessary since rpsec -v2.99.0 extracted `its {}` type of assertion in the process of migrating to Rspec 3.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "rubygems"
 require "rspec"
+require "rspec/its"
 
 # gem install redgreen for colored test output
 begin require "redgreen" unless ENV['TM_CURRENT_LINE']; rescue LoadError; end
